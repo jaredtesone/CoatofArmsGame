19c8a31bd15bf51539b76a78a499641fe4e64b63
Jonathan Stansfield <jonathanrstansfield@gmail.com>
2018-10-13 18:03:12 -0400

    Tilesheet and Map1
    For testing

---
+++ assets/TileSheetv2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="TileSheetv2" tilewidth="125" tileheight="125" tilecount="100" columns="10">
+ <image source="TileSheetv2.png" width="1250" height="1250"/>
+</tileset>
