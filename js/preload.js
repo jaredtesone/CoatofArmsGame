f9f891eb02735616adc4b25e1f4d1a52f3b5effa
Eric Lujan <lujan.eric@gmail.com>
2018-10-16 13:43:21 -0400

    Dragon Fire Sound
    Dragon Fire Sound

--- js/preload.js
+++ js/preload.js
@@ -45,7 +45,7 @@ preloadState.prototype.preload = function() {
 };
 
 preloadState.prototype.create = function() {
-	game.state.start("LevelFour");
+	game.state.start("LevelOne");
 };
 
 preloadState.prototype.update = function() {
